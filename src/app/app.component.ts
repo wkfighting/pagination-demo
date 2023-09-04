daea978c9db0c2ffcc4388b0df4ee8d9bb6a1acd
wukai <is.wukai@outlook.com>
2023-09-05 01:43:18 +0800

    feat: pagination demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pagination-demo';
+export class AppComponent implements OnInit {
+  total = 100;
+  curPage = 1;
+  pageSize = {
+    size: 10,
+    options: [10, 20, 50, 100],
+  };
+
+  list: any[] = [];
+  displayed: any[] = [];
+
+  get totalPages(): number {
+    return Math.ceil(this.total / this.pageSize.size);
+  }
+
+  ngOnInit(): void {
+    this.list = new Array(this.total)
+      .fill(0)
+      .map((item, index) => `Index: ${index + 1}`);
+    console.log('list:', this.list);
+  }
+
+  prePage() {
+    this.curPage--;
+  }
+
+  nextPage() {
+    this.curPage++;
+  }
+
+  jumpToPage() {
+    if (this.curPage < 1 || this.curPage > this.totalPages) return;
+    console.log('jumpToPage:', this.curPage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
